e034c1662b02fe2e92971ef4139db9a646dd057d
mardi2020 <mardi@khu.ac.kr>
2021-01-22 18:16:01 +0900

    update client/server

--- socket/client.py
+++ socket/client.py
@@ -2,27 +2,65 @@ from socket import *
 from ctypes import *
 
 TOTAL = 10
-PORT = 12345
+PORT = 12345 
 
-class ParkingLot(Structure) :
+class Car(Structure) :
     _fields_ = [
         ("parkingLotNumber", c_int),
         ("occupiedNumber", c_int),
-        ("totalNumber", c_int),
-        ("occupiedarea", c_int * TOTAL)
+        ("carnumber", c_char * 100)
             ]
+
+class message_format(Structure) :
+    _fields_ = [
+        ("message_id", c_short),
+        ("len", c_int),
+        ("data", c_char * 256)
+    ]
+
+class ParkingLot(Structure) :
+    _fields_ = [
+        ("number", c_int),
+        ("address", c_char * 256),
+        ("empty", c_int), 
+        ("space", c_int),
+        ("occupiedarea", c_int * TOTAL)
+    ]
+
+
+        
 
 socket_client = socket(AF_INET, SOCK_STREAM)
 socket_client.connect(('127.0.0.1', PORT))
 
-while True :
+# 차량 정보를 얻기 위한 메시지
+mess = message_format()
+mess.message_id = 2 # 주차장 : 1 차량 : 2
+data ="11가 1111"
+mess.len = len(data)
+mess.data = bytes(data+"\0",'utf-8')
+
+socket_client.send(mess)
+
+
+if mess.message_id == 1 :
     buff = socket_client.recv(sizeof(ParkingLot))
-    print(buff)
     pl = ParkingLot.from_buffer_copy(buff)
-    print(pl.parkingLotNumber)
-    print(pl.occupiedNumber)
-    print(pl.totalNumber)
-    print(pl.occupiedarea)
+    print(buff)
+    print(pl)
+    print("주차장 번호 : ", pl.number)
+    print("주차장 주소 : ", pl.address.decode('utf-8'))
+    print("비어있는 자리 수 : ",pl.empty)
+    print("총 주차 자리수 : ",pl.space)
+    print("주차 배열 : ",pl.occupiedarea[:])
+
+elif mess.message_id == 2 :
+    buff = socket_client.recv(sizeof(Car))
+    car = Car.from_buffer_copy(buff)
+    print("주차장 번호 : ",car.parkingLotNumber)
+    print("주차된 차량 위치 : ",car.occupiedNumber)
+    print("차번호 :", car.carnumber.decode('utf-8'))
+
 
 print("Closing socket")
 socket_client.close()
--- socket/server.cpp
+++ socket/server.cpp
@@ -1,52 +1,61 @@
 #include <iostream>
-
+#include <time.h> //차량 등록시 시간 추가
+#include <cstring>
+#include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h> // 소켓 지원을 위한 각종 함수
-#include <cstring>
 #include <unistd.h> // 시스템 함수
-
 #include <pthread.h>
 #include <signal.h>
-
+#include </usr/include/mariadb/mysql.h>
+#include <iomanip>
+#include <vector>
+#include <sstream>
 #define endl '\n'
-#define MAX 1024
 #define PORT_NUM 12345
 #define TOTAL 10
+// mariaDB
+#define HOST "localhost"
+#define USER "root"
+#define PASSWORD "test"
+#define DATABASE "parking"
+#define PORT 3306
+#define TABLENAME "car"
 
-int Sockfd;
-struct ParkingLot{
+struct Car{
     int parkingLotNumber;
     int occupiedNumber;
-    int totalNumber = TOTAL;
+    char carnumber[100] = {0,};
+    //int totalNumber = TOTAL;
+    //int occupiedarea[TOTAL] = {0,};
+};
+struct ParkingLot{
+    int number; // 주차장의 번호
+    char address[256]; // 주차장 주소
+    int empty; // 주차장의 빈 자리
+    int space; // 주차장 총 주차 자리
     int occupiedarea[TOTAL] = {0,};
 };
+std::vector<MYSQL_ROW> car_vector; // 차량 정보 저장
+std::vector<MYSQL_ROW> parkinglot_vector; // 주차장정보 저장
+int mess_id;
+char mess_data[256];
 
-void messagefunc(ParkingLot &pl, int x, int y, int z, int* arr){
-    pl.parkingLotNumber = x;
-    pl.occupiedNumber = y;
-    pl.totalNumber = z;
-    memcpy(pl.occupiedarea, arr, sizeof(int)*TOTAL);
-}
+struct message_format {
+    short message_id; // tag
+    int len; // length
+    char data[256]; // data
+};
 
-
-void *thread_function(void *data){
-    int sockfd = *((int *) data);
-    socklen_t addrlen;
-    struct sockaddr_in client_addr;
-    addrlen = sizeof(client_addr);
-
-    ParkingLot PL;
-    int arr[TOTAL] = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
-    messagefunc(PL, 1, 2, TOTAL, arr);
-    int s = (send(sockfd, (ParkingLot *)& PL, sizeof(PL), 0));
-
-    close(sockfd);
-
-    std::cout<<"Client thread end"<<endl;
-}
-
+void sig_handler(int signo);
+void ComputeOccupiedArea(std::vector<MYSQL_ROW> row, int arr[]);
+void setmessagecar(Car &car, int x, int y, char num[]);
+void setmessageparkinglot(ParkingLot &pl, int num, char add[], int emt, int space, int ary[]);
+void *thread_function1(void *data);
+void *thread_function2(void *data);
+MYSQL_RES* UpdateOccupiedNumber(MYSQL* Connptr, int parkinglotnum);
 
 int main(){
     int listen_fd, client_fd;
@@ -54,24 +63,48 @@ int main(){
     pthread_t thread_id;
     struct sockaddr_in server_addr, client_addr;
 
+    MYSQL Conn; // mysql 정보를 담을 구조체
+    MYSQL* ConnPtr = nullptr; // mysql
+    MYSQL_RES* res; // 쿼리 결과를 담는 구조체 포인터
+    MYSQL_ROW row; // 쿼리 결과의 행을 담는 구조체
+    int status; // 쿼리 요청 후 결과
+
+    /* mysql 연결 */
+    mysql_init(&Conn);  // mysql 초기화
+    // 한글 깨짐 설정
+    mysql_options(&Conn, MYSQL_SET_CHARSET_NAME, "utf8");
+    mysql_options(&Conn, MYSQL_INIT_COMMAND, "SET NAMES utf8");
+
+    ConnPtr = mysql_real_connect(&Conn, HOST, USER, PASSWORD, DATABASE, PORT, (char*)NULL, 0);
+    
+    if (ConnPtr == nullptr){
+        perror("connection error");
+        return 1;
+    }
+
+    mysql_query(ConnPtr, "SELECT * FROM car");
+    res = mysql_store_result(ConnPtr);
+    while((row = mysql_fetch_row(res)) != NULL){
+        car_vector.push_back(row);
+    }
+    res = UpdateOccupiedNumber(ConnPtr, 1);
+
+    /* socket 연결 */
     if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         return 1;
     
     memset((void *)&server_addr, 0x00, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = PORT_NUM;
+    server_addr.sin_port = htons(PORT_NUM);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // htonl : IP는 32비트이므로
-    // INADDR_ANY : 컴퓨터에 존재하는 랜카드 중 사용 가능한 랜카드의 ip 주소 사용
     
     // bind(소켓 디스크립터, IP, 주소 길이)
     if(::bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
         perror("bind() error");
         return 1;
     }
-    
     std::cout<<"Server is started at port "<<PORT_NUM<<endl;
 
-    // 요청이 들어오면 수락할 수 있는 대기상태
     // listen(소켓 디스크립터, 연결 대기열의 크기)
     if(listen(listen_fd, 5) < 0){
         perror("listen() error");
@@ -80,20 +113,120 @@ int main(){
 
     while(1){
         addrlen = sizeof(client_addr);
-        // 서버 소켓에 클라이언트를 연결하는 함수
-        // accept(소켓 디스클비터, 클라이언트 주소 정보의 구조체, 2번째 인자값의 길이)
         client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &addrlen);
+
         if(client_fd < 0)
             std::cout<<"accept() error"<<endl;
         else{
-            // pthread_create(스레드의 id 저장을 위한 변수의 주소 전달, 
-            // 스레드에 부여할 특성 정보의 전달을 위한 매개 변수[NULL : 기본적인 특성의 스레드 생성],
-            // 스레드의 메인 함수 역할을 하는 별도 실행 흐름의 시작이 되는 함수의 주소 값 전달,
-            // 세 번째 인자를 통해 등록된 함수가 호출될 때 전달할 인자의 정보를 담고 있는 변수의 주소 전달)
-            
-            pthread_create(&thread_id, NULL, thread_function, (void *)&client_fd);
+            message_format message;
+            int recv_ = recv(client_fd, (message_format *)&message, sizeof(message), 0);
 
+            if(recv_ > 0){
+                std::cout<<"받은 메시지 : "<<message.message_id<<", "<<message.data<<", "<<message.len<<endl;
+            }
+            mess_id = message.message_id;
+            strcpy(mess_data, message.data);
+
+            if(1 == message.message_id){   // 주차장
+                mysql_query(ConnPtr, "SELECT * FROM parkinglot");
+                res = mysql_store_result(ConnPtr);
+                while((row = mysql_fetch_row(res)) != NULL){
+                    parkinglot_vector.push_back(row);
+                }
+                pthread_create(&thread_id, NULL, thread_function1, (void *)&client_fd);
+            }
+            else if (2 == message.message_id){ // 차량
+                pthread_create(&thread_id, NULL, thread_function2, (void *)&client_fd);
+            }
+
+            memset(message.data, 0x00, sizeof(message.data));
             pthread_detach(thread_id);
         }
     }
+    
+    mysql_free_result(res);
+}
+
+void ComputeOccupiedArea(std::vector<MYSQL_ROW> row, int arr[]){
+    int n = row.size();
+    for(int i = 0 ;i < n ;i++){
+        if (row[i][5] != NULL)
+            arr[atoi(row[i][5])-1] = 1;
+    }
+}
+void setmessagecar(Car &car, int x, int y, char num[]){
+    car.parkingLotNumber = x;
+    car.occupiedNumber = y;
+    strcpy(car.carnumber, num);
+    //car.totalNumber = z;
+    //memcpy(car.occupiedarea, arr, sizeof(int)*TOTAL);
+}
+void setmessageparkinglot(ParkingLot &pl, int num, char add[], int emt, int space, int ary[]){
+    pl.number = num;
+    strcpy(pl.address, add);
+    pl.empty = emt;
+    pl.space = space;
+    memcpy(pl.occupiedarea, ary, sizeof(int)*TOTAL);
+}
+void *thread_function1(void *data){
+    int sockfd = *((int *) data);
+    socklen_t addrlen;
+    struct sockaddr_in client_addr;
+    addrlen = sizeof(client_addr);
+    ParkingLot pl;
+
+    int arr[TOTAL] = {0,}; // 자동차가 주차된 위치 저장하는 배열
+    ComputeOccupiedArea(car_vector, arr); 
+    int num = 0; // 이미 주차된 자리 수
+    for(auto e : car_vector){
+        if(e[5] != NULL)
+            num += 1;
+    }
+
+    setmessageparkinglot(pl, atoi(mess_data), parkinglot_vector[atoi(mess_data)][1], TOTAL-num, TOTAL, arr);
+    int s = (send(sockfd, (ParkingLot *)& pl, sizeof(pl), 0));
+    if (s < 0)
+        std::cout<<"send() error"<<endl;
+
+    close(sockfd);
+    std::cout<<"주차장 thread end"<<endl;
+}
+
+void *thread_function2(void *data){
+    int sockfd = *((int *) data);
+    socklen_t addrlen;
+    struct sockaddr_in client_addr;
+    addrlen = sizeof(client_addr);
+    Car car;
+
+    int parkinglotnum = 0, occupiednum = 0;
+    for (auto e : car_vector){
+        if (strcmp(e[0], mess_data) == 0){
+            if(e[5] != NULL && e[6] != NULL){
+                parkinglotnum = atoi(e[6]);
+                occupiednum = atoi(e[5]);
+            }
+        }
+    }
+    
+    setmessagecar(car, parkinglotnum, occupiednum, mess_data);
+    int s = (send(sockfd, (Car *)& car, sizeof(car), 0));
+
+    close(sockfd);
+    std::cout<<"차량 thread end"<<endl;
+}
+
+// Car 테이블의 location_p 변수를 count하여 parkinglot 테이블 occupied에 반영
+MYSQL_RES* UpdateOccupiedNumber(MYSQL* Connptr, int parkinglotnum){
+    std::string String = static_cast<std::ostringstream*>( &(std::ostringstream() << parkinglotnum))->str();
+    MYSQL_RES* res;
+    std::string query = "UPDATE parkinglot SET occupied=(SELECT COUNT(*) FROM car WHERE location_p = "+ String  +") WHERE number = "+ String;
+    mysql_query(Connptr, query.c_str());
+    res = mysql_store_result(Connptr);
+
+    return res;
+}
+
+void sig_handler(int signo){
+    
 }
